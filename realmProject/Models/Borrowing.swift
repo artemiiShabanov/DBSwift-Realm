a658867e7924ce60dbb56b353c87bdbabd92ad00
Artemii <artemshabanov7@mail.ru>
2018-04-25 20:24:16 +0300

    Add files for models and create classes for them

---
+++ realmProject/Models/Borrowing.swift
@@ -0,0 +1,21 @@
+//
+//  Borrowing.swift
+//  realmProject
+//
+//  Created by Artemii Shabanov on 25.04.2018.
+//  Copyright © 2018 C3G9. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Borrowing: Object {
+    @objc dynamic let book: Book? = nil
+    @objc dynamic let friend: Friend? = nil
+    @objc dynamic let borrowDate = NSDate()
+    
+    @objc dynamic var isLost = false
+    @objc dynamic var isDamaged = false
+    @objc dynamic var returnDate = NSDate()
+    @objc dynamic var comment: String? = nil
+}
