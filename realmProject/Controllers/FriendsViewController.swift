832402f634333bd5cf687ae31720e98fc4027c5a
Juliett13 <k.julia_13@mail.ru>
2018-04-26 23:34:35 +0300

    Fix controllers and create classes for them

---
+++ realmProject/Controllers/FriendsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FriendsViewController.swift
+//  realmProject
+//
+//  Created by Kuroyan Juliett on 26.04.18.
+//  Copyright © 2018 C3G9. All rights reserved.
+//
+
+import UIKit
+
+class FriendsViewController: UITableViewController {
+    
+}
+
---
+++ realmProject/Controllers/PublishingHousesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PublishingHousesViewController.swift
+//  realmProject
+//
+//  Created by Kuroyan Juliett on 26.04.18.
+//  Copyright © 2018 C3G9. All rights reserved.
+//
+
+import UIKit
+
+class PublishingHousesViewController: UITableViewController {
+    
+}
